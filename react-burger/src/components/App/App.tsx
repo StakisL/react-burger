d3797f12bb1cbd52d2367644b669136c83c635ce
stanislav <s.lebel@dodopizza.com>
2022-10-02 18:57:15 +0300

    Add temporary header markup

--- react-burger/src/components/App/App.tsx
+++ react-burger/src/components/App/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppHeader from '../AppHeader/AppHeader.jsx'
 import BurgersData from '../../utils/data.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader/>
     </div>
   );
 }
--- react-burger/src/components/AppHeader/AppHeader.jsx
+++ react-burger/src/components/AppHeader/AppHeader.jsx
@@ -1,8 +1,39 @@
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
 
 function AppHeader() {
 
-    return ({
-
-    });
+    return (
+        <header className="header"> 
+            <div className="header-container">
+                <section className="header-block">
+                    <a className="nav-button" href="1">
+                        <BurgerIcon className="nav-button__logo"/>
+                        <p className="nav-button__text">
+                            Конструктор
+                        </p>
+                    </a>
+                    <a className="nav-button" href="2">
+                        <ListIcon className="nav-button__logo"/>
+                        <p>
+                            Лента заказов
+                        </p>
+                    </a>
+                </section>
+                <section className="header-block ">
+                    <Logo className="logo"/>
+                </section>
+                <section className="header-block">
+                    <a className="nav-button" href="3">
+                        <ProfileIcon className="nav-button__logo"/>
+                        <p className="nav-button__text">
+                            Личный кабинет
+                        </p>
+                    </a>
+                </section>
+           </div>
+        </header>
+    );
 }
+
+export default AppHeader;
