58230c17f95d34417a2fc905a2f26b6b73c6fae4
stanislav <s.lebel@dodopizza.com>
2022-10-02 13:38:25 +0300

    move files to folders

--- react-burger/src/components/App/App.tsx
+++ react-burger/src/components/App/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BurgersData from '../../utils/data.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
