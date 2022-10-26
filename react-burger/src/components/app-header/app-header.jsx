1a5367743a8d55248864d3deadfd90a8f93eaf2f
stanislav <s.lebel@dodopizza.com>
2022-10-26 17:54:29 +0300

    WIP: Complete header without hover.

--- react-burger/src/components/app-header/app-header.jsx
+++ react-burger/src/components/app-header/app-header.jsx
@@ -1,43 +1,48 @@
-import { render } from "@testing-library/react";
-import { BurgerIcon, ListIcon, Logo, ProfileIcon, Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
 import './app-header.css'
 
 class AppHeader extends React.Component {
 
+    state = {type: "secondary"};
+
+    onMouseEnter = () => {
+        this.setState({type: this.state.type = "primary"})
+    }
+
+    onMouseLeave = () => {
+        this.setState({type: this.state.type = "secondary"})
+    }
+
     render(){
     return (
         <header className="header"> 
             <nav className="header-container">
-              <ul className="nav-list">
-                  <li className="nav-item">
-                      <a className="nav-link" href="1">
-                          <BurgerIcon className="nav-link__logo ml-20" type="secondary" />
-                          <p className="nav-link__text">
-                              Конструктор
-                          </p>
-                      </a>
-                  </li>
-                  <li className="nav-item">
-                      <a className="nav-link" href="2">
-                          <ListIcon className="nav-link__logo" type="secondary" />
-                          <p className="nav-link__text"> 
-                              Лента заказов
-                          </p>
-                      </a>
-                  </li>
-                  <li className="nav-item">
-                      <Logo className="header__logo"/>
-                  </li>
-                  <li >
-                      <a className="nav-link" href="3">
-                          <ProfileIcon className="nav-link__logo" type="secondary"/>
-                          <p className="nav-link__text">
-                              Личный кабинет
-                          </p>
-                      </a>
-                  </li>
-              </ul>
+              <section className="nav-section ml-10 mr-10">
+                <div className="nav-link-container mt-4 mb-4">
+                    <a className="nav-link" href="1">
+                        <BurgerIcon className="nav-link__logo ml-20" type="secondary" />
+                        <p className="nav-link__text">
+                            Конструктор
+                        </p>
+                    </a>
+                    <a className="nav-link" href="2" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+                        <ListIcon className="nav-link__logo" type={this.state.type}/>
+                        <p className="nav-link__text"> 
+                            Лента заказов
+                        </p>
+                    </a>
+                </div>
+                <Logo className="header__logo mt-6 mb-6"/>
+                <div className="nav-link-container mt-4 mb-4">
+                    <a className="nav-link" href="3">
+                        <ProfileIcon className="nav-link__logo" type="secondary"/>
+                        <p className="nav-link__text">
+                            Личный кабинет
+                        </p>
+                    </a>
+                </div>
+                </section>
             </nav>
         </header>
     );
