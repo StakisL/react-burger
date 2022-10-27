a648ffb490122e52b0f5f090c0facc13b6c7fca9
stanislav <s.lebel@dodopizza.com>
2022-10-27 09:42:33 +0300

    Add BurgerConstructor markup without styles.

--- react-burger/src/components/burger-constructor/burger-constructor.jsx
+++ react-burger/src/components/burger-constructor/burger-constructor.jsx
@@ -1,25 +1,35 @@
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgersData from '../../utils/data';
 
 function BurgerConstructor() {
     return (
-        <div>
-            <section>
-                <ul>
-                    {BurgersData.map((burger) => (
-                        <ConstructorElement text={burger.name}
+        <section className="constructor">
+            <ul className="ingredients-list">
+                {BurgersData.map((burger) => (
+                    <div className="ingredients-list-item">
+                        <DragIcon className="ingredients-list-item__icon" type="primary" size=""/>
+                        <ConstructorElement className="ingredients-list-item__description"
+                                            text={burger.name}
                                             type={burger.type}
                                             price={burger.price}
                                             thumbnail={burger.image}
                                             />
-                    ))} 
-                </ul>
-            </section>
-            <section>
+                        
+                    </div>
+                ))} 
+            </ul>
+            <section className="order-info">
+                <section className="order-info-price">
+                    <p className="order-info-price__total">
 
+                    </p>
+                    <CurrencyIcon className="order-info-price__icon" type="primary"/>
+                </section>
+                <Button type="primary" size="medium">
+                    Оформить заказ
+                </Button>
             </section>
-        </div>
-        
+        </section>
     );
 }
 
