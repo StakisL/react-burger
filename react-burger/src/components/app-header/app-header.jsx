356ce6333b3f79b1b849e594269804cf24849019
stanislav <s.lebel@dodopizza.com>
2022-10-30 21:06:42 +0300

    Minor refactoring

--- react-burger/src/components/app-header/app-header.jsx
+++ react-burger/src/components/app-header/app-header.jsx
@@ -1,5 +1,4 @@
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import React from "react";
 import './app-header.css'
 
 function AppHeader() {
