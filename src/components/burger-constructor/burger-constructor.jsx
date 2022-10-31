a3bd4f249a610223aa1fa2ffc2f26cb85de915f6
stanislav <s.lebel@dodopizza.com>
2022-10-31 07:33:31 +0300

    Remove useless findLast()

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,76 +1,76 @@
-import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React from 'react';
-import PropTypes from 'prop-types';
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import React from 'react'
+import PropTypes from 'prop-types'
 import './burger-constructor.css'
 import burgerPropTypes from '../../prop-types.jsx'
 
 /*eslint no-dupe-keys: "off"*/
 BurgerConstructor.propTypes = {
-    data: PropTypes.array.isRequired,
-    data: PropTypes.arrayOf(burgerPropTypes).isRequired
+	data: PropTypes.array.isRequired,
+	data: PropTypes.arrayOf(burgerPropTypes).isRequired,
 }
 
 function BurgerConstructor(props) {
-    const topBun = props.data.find(element => element.type === 'bun');
-    const bottomBun = props.data.findLast(element => element.type === 'bun')
-    const ingredients = props.data.filter(element => element.type !== 'bun')
+	const bun = props.data.find((element) => element.type === 'bun')
+	const ingredients = props.data.filter((element) => element.type !== 'bun')
 
-    const findTotalPrice = React.useMemo(() => {
-        let sum = 0;
-        props.data.forEach(element => {
-            sum += element.price;
-        });
-        return sum;
-    }, [props.data]);
+	const findTotalPrice = React.useMemo(() => {
+		let sum = 0
+		props.data.forEach((element) => {
+			sum += element.price
+		})
+		return sum
+	}, [props.data])
 
-    return (
-        <section className="constructor mt-25 mb-10">
-            <span className="constructor-list-item ml-5">
-                <ConstructorElement className="constructor-list-item__description"
-                                    text={topBun.name}
-                                    type={topBun.type}
-                                    price={topBun.price}
-                                    thumbnail={topBun.image}
-                                    isLocked={true}
-                                    />
-            </span>
-            <ul className="constructor-list">
-                {ingredients.map((ingredient) => (
-                    <section className="constructor-list-item mt-4 mb-4 mr-1 ml-1" key={ingredient._id}>
-                        <span className="constructor-list-item__icon-container mr-1">
-                          <DragIcon className="constructor-list-item__icon" type="primary"/>
-                        </span>
-                        <ConstructorElement className="constructor-list-item__description"
-                                            text={ingredient.name}
-                                            type={ingredient.type}
-                                            price={ingredient.price}
-                                            thumbnail={ingredient.image}
-                                            />
-                    </section>
-                ))} 
-            </ul>
-            <span className="constructor-list-item ml-5 mb-10">
-                <ConstructorElement className="constructor-list-item__description"
-                                    text={bottomBun.name}
-                                    type={bottomBun.type}
-                                    price={bottomBun.price}
-                                    thumbnail={bottomBun.image}
-                                    isLocked={true}
-                                    />
-            </span>
-            <section className="order-info">
-                <section className="order-info-price mr-10 ml-10">
-                    <p className="order-info-price__total text_type_digits-medium mr-1">
-                        {findTotalPrice}
-                    </p>
-                    <CurrencyIcon className="order-info-price__icon" type="primary"/>
-                </section>
-                <Button type="primary" size="medium">
-                    Оформить заказ
-                </Button>
-            </section>
-        </section>
-    );
+	return (
+		<section className="constructor mt-25 mb-10">
+			<span className="constructor-list-item ml-5">
+				<ConstructorElement
+					className="constructor-list-item__description"
+					text={bun.name}
+					type={bun.type}
+					price={bun.price}
+					thumbnail={bun.image}
+					isLocked={true}
+				/>
+			</span>
+			<ul className="constructor-list">
+				{ingredients.map((ingredient) => (
+					<section className="constructor-list-item mt-4 mb-4 mr-1 ml-1" key={ingredient._id}>
+						<span className="constructor-list-item__icon-container mr-1">
+							<DragIcon className="constructor-list-item__icon" type="primary" />
+						</span>
+						<ConstructorElement
+							className="constructor-list-item__description"
+							text={ingredient.name}
+							type={ingredient.type}
+							price={ingredient.price}
+							thumbnail={ingredient.image}
+						/>
+					</section>
+				))}
+			</ul>
+			<span className="constructor-list-item ml-5 mb-10">
+				<ConstructorElement
+					className="constructor-list-item__description"
+					text={bun.name}
+					type={bun.type}
+					price={bun.price}
+					thumbnail={bun.image}
+					isLocked={true}
+				/>
+			</span>
+			<section className="order-info">
+				<section className="order-info-price mr-10 ml-10">
+					<p className="order-info-price__total text_type_digits-medium mr-1">{findTotalPrice}</p>
+					<CurrencyIcon className="order-info-price__icon" type="primary" />
+				</section>
+				<Button type="primary" size="medium">
+					Оформить заказ
+				</Button>
+			</section>
+		</section>
+	)
 }
 
-export default BurgerConstructor;
+export default BurgerConstructor
