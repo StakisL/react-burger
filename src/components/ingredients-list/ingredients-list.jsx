2dacabd163652b6cc4207cfcb1aaf45eea1ff28f
stanislav <s.lebel@dodopizza.com>
2022-11-03 13:38:10 +0300

    Move ingredients list on module css

--- src/components/ingredients-list/ingredients-list.jsx
+++ src/components/ingredients-list/ingredients-list.jsx
@@ -1,5 +1,5 @@
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import './ingredients-list.css'
+import styles from './ingredients-list.module.css'
 import PropTypes from 'prop-types'
 import burgerPropTypes from '../../utils/prop-types.jsx'
 
@@ -10,17 +10,22 @@ IngredientsList.propTypes = {
 
 function IngredientsList(props) {
 	return (
-		<section className="ingredients-list" ref={props.ref}>
-			<h2 className="ingredients-list-header ml-8">{props.type}</h2>
-			<ul className="ingredients-list-items-container">
+		<section className={styles.ingredients_list} ref={props.ref}>
+			<h2 className={`${styles.ingredients_list_header} ml-8`}>{props.type}</h2>
+			<ul className={styles.ingredients_list_items_container}>
 				{props.ingredients.map((ingredient) => (
-					<section className="ingredients-list-item" key={ingredient._id}>
-						<img className="ingredients-list-item__image" src={ingredient.image} alt="" />
-						<span className="ingredients-list-item__price">
-							<p className="ingredients-list-item__count text_type_digits-default"> {ingredient.price}</p>
-							<CurrencyIcon className="ingredients-list-item__currency" type="primary" />
+					<section className={styles.ingredients_list_item} key={ingredient._id}>
+						<img className={styles.ingredients_list_item__icon} src={ingredient.image} alt="" />
+						<span className={styles.ingredients_list_item__price}>
+							<p className={`${styles.ingredients_list_item__count}  text_type_digits-default`}>
+								{' '}
+								{ingredient.price}
+							</p>
+							<CurrencyIcon className={styles.ingredients_list_item__currency} type="primary" />
 						</span>
-						<p className="ingredients-list-item__name text_type_main-default">{ingredient.name}</p>
+						<p className={`${styles.ingredients_list_item__name}  text_type_main-default`}>
+							{ingredient.name}
+						</p>
 					</section>
 				))}
 			</ul>
