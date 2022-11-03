2e8054174645c92eaa9aae7ccd379c7bf84782ea
stanislav <s.lebel@dodopizza.com>
2022-11-03 13:21:32 +0300

    Move app jsx on module css.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './app.css'
+import styles from './app.module.css'
 import AppHeader from '../app-header/app-header.jsx'
 import BurgerConstructor from '../burger-constructor/burger-constructor'
 import BurgerIngredients from '../burger-ingredients/burger-ingredients'
@@ -8,8 +8,8 @@ import BurgersData from '../../utils/data'
 function App() {
 	return (
 		<React.Fragment>
-			<AppHeader className="app-header" />
-			<div className="app-body">
+			<AppHeader className={styles.app_header} />
+			<div className={styles.app_body}>
 				<BurgerIngredients data={BurgersData} />
 				<BurgerConstructor data={BurgersData} />
 			</div>
