402d2052cf023f207838222426cf7f375c1c7ed6
stanislav <s.lebel@dodopizza.com>
2022-11-06 14:20:28 +0300

    Fix markup on details.

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -9,13 +9,11 @@ function IngredientDetails(props) {
 	return (
 		<section className={detailStyles.ingredient_detail}>
 			<img className={detailStyles.ingredient_image} src={props.ingredient.image_large} alt="" />
-			<p className={`${detailStyles.ingredient_name} text_type_main-default mt-4 mb-4`}>
-				{props.ingredient.name}
-			</p>
-			<ul className={`${detailStyles.ingredient_info_list} mt-4 mb-15`}>
+			<p className={`${detailStyles.ingredient_name} text_type_main-medium mt-4 mb-4`}>{props.ingredient.name}</p>
+			<ul className={`${detailStyles.ingredient_info_list} mt-8 mb-15`}>
 				<li className={`${detailStyles.ingredient_info_list__item} mr-5`}>
 					<p
-						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-small text_color_inactive`}
+						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-default text_color_inactive`}
 					>
 						Калории, ккал
 					</p>
@@ -27,7 +25,7 @@ function IngredientDetails(props) {
 				</li>
 				<li className={`${detailStyles.ingredient_info_list__item} mr-5`}>
 					<p
-						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-small text_color_inactive`}
+						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-default text_color_inactive`}
 					>
 						Белки, г
 					</p>
@@ -39,7 +37,7 @@ function IngredientDetails(props) {
 				</li>
 				<li className={`${detailStyles.ingredient_info_list__item} mr-5`}>
 					<p
-						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-small text_color_inactive`}
+						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-default text_color_inactive`}
 					>
 						Жиры, г
 					</p>
@@ -51,7 +49,7 @@ function IngredientDetails(props) {
 				</li>
 				<li className={`${detailStyles.ingredient_info_list__item} mr-5`}>
 					<p
-						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-small text_color_inactive`}
+						className={` ${detailStyles.ingredient_info_list__item_text} text_type_main-default text_color_inactive`}
 					>
 						Углеводы, г
 					</p>
--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,14 +1,12 @@
-import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import detailStyles from './order-details.module.css'
+import logo from '../../images/done.png'
 
 function OrderDetails() {
 	return (
-		<section className={`${detailStyles.order_details} mt-15 mb-30`}>
+		<section className={`${detailStyles.order_details} mt-12 mb-15`}>
 			<p className={`${detailStyles.order_number} text_type_digits-large`}> 034536</p>
 			<p className={`${detailStyles.order_identifier} text_type_main-default mt-8`}>идентификатор заказа</p>
-			<span className={`${detailStyles.order_conteiner} mt-15 mb-15`}>
-				<CheckMarkIcon className={`${detailStyles.order_conteiner__icon} `} type="primary" />
-			</span>
+			<img className={`${detailStyles.order_conteiner} mt-15 mb-15`} src={logo} alt="Logo" />
 			<p className={`${detailStyles.order_accepted_text} text_type_main-default mb-2`}>
 				Ваш заказ начали готовить
 			</p>
